48a22d179861897b0e1ebf6ffcf7d15617e44ca0
Youcef <youcef.zahariou@gmail.com>
2019-07-25 02:55:34 +0100

    added more styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ManageTodos from './components/ManageTodos'
 import Todos from "./components/Todos";
-
+import './App.css'
 
 function App() {
   return (
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import withConnect from "../withConnect";
 import Todo from "./Todo";
-import {Container} from 'react-bootstrap';
+import { Container, Jumbotron } from "react-bootstrap";
 
 const Todos = ({ todos, removeTodo }) => {
   return (
-    <Container>
-      {todos.map((todo, index) => {
-        return <Todo key={index} id={index} text={todo} removeTodo={removeTodo}/>;
-      })}
-    </Container>
+    <>
+      <Container>
+        <Jumbotron>
+          <h1 className="header">Tasks to do:</h1>
+          {todos.map((todo, index) => {
+            return <Todo key={index} id={index} text={todo} removeTodo={removeTodo} />;
+          })}
+        </Jumbotron>
+      </Container>
+    </>
   );
 };
 
